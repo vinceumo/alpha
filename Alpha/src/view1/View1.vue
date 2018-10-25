545e6b444105b36081e4738eef9f11a9936629aa
Vincent Humeau <hello@vincent-humeau.com>
2018-10-25 09:59:01 +0100

    Run lint

--- Alpha/src/view1/View1.vue
+++ Alpha/src/view1/View1.vue
@@ -5,12 +5,12 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import { Component, Prop } from "vue-property-decorator";
+import Vue from 'vue';
+import { Component, Prop } from 'vue-property-decorator';
 
 @Component
 export default class View1Component extends Vue {
-  @Prop({ default: "This is View 1" })
+  @Prop({ default: 'This is View 1' })
   public message: string;
 }
 </script>

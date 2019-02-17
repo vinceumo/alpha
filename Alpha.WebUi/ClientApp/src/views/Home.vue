7165c5d74fa425f9abe231362e8a6afd90a4749a
Vincent Humeau <hello@vincent-humeau.com>
2019-02-17 16:52:26 +0000

    Move proj to sub proj

---
+++ Alpha.WebUi/ClientApp/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="home">
+        <MainBoard />
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
+</template>
+
+<script>
+// @ is an alias to /src
+    import HelloWorld from "@/components/HelloWorld.vue";
+    import MainBoard from '@/components/MainBoard.vue';
+
+export default {
+  name: "home",
+  components: {
+      HelloWorld,
+      MainBoard
+  }
+};
+</script>
